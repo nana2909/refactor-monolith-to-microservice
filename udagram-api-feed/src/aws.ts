8501608e9dba315d0db70e73931ff4323e7dc045
Minh Ngoc <minhngoclenguyen@gmail.com>
2022-12-22 08:34:43 +0700

    log-media

--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -26,7 +26,7 @@ export function getGetSignedUrl( key: string ): string {
 // Generates an AWS signed URL for uploading objects
 export function getPutSignedUrl( key: string ): string {
   const signedUrlExpireSeconds = 60 * 5;
-
+  console.log(config.aws_media_bucket);
   return s3.getSignedUrl('putObject', {
     Bucket: config.aws_media_bucket,
     Key: key,
